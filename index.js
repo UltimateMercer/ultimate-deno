0790cb1477b61388e3b385118cc6fcca94d8bd9f
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2021-11-17 16:50:44 -0300

    test jsx

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+
+// /** @jsx h */
+// /// <reference no-default-lib="true"/>
+// /// <reference lib="dom" />
+// /// <reference lib="dom.asynciterable" />
+// /// <reference lib="deno.ns" />
+
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX! By Ultimate Mercer</title>
+      </head>
+      <body>
+        <h1>Hello Ultimate! This is a test with Deno Deploy!</h1>
+      </body>
+    </html>
+  );
+}
 
 function handler(req) {
-  return new Response("Hello Ultimate! This is a test with Deno Deploy!");
+  const html = renderSSR(<App />);
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
 }
 
 console.log("Listening on http://localhost:8000");
